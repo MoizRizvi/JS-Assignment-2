2f8eed39a00d12d21cc367c606fc5298a5c0261a
Moiz Rizvi <moizrizvi7@gmail.com>
2020-06-21 20:38:26 +0500

    JS Assignment 2

---
+++ app35-38.js
@@ -0,0 +1,101 @@
+//Task 1
+/*
+function displayDate() {
+ date = new Date();
+ document.write(date);
+}
+
+var a = displayDate();
+*/
+
+//Task 2
+/*
+function greet () {
+var firstName = prompt("Please enter your First Name");
+var lastName = prompt("Please enter your Last Name");
+alert("Hello "+firstName+" "+lastName);
+}
+
+var a = greet();
+*/
+
+//Task 3
+/*
+var num=prompt("Enter two numbers you want to add and seperate them by a comma","60,170"),
+     num1=num.split(",")[0],
+     num2=num.split(",")[1];
+var sum = num1 + num2;
+alert("The sum is "+sum);
+*/
+/*
+function sum () {
+	var m = prompt("Enter a number ");
+	var n = prompt("Enter the number you want to add ");
+	var num1 = parseInt(m), num2 = parseInt(n);
+	var a = num1 + num2
+	alert(a);
+}
+var b = sum();
+*/
+
+//Task 4
+/*
+function calculate () {
+
+}
+*/
+
+
+//Task 5 
+/*
+function square() {
+	var a = prompt("Enter the number you want the square of");
+	var n = parseInt(a);
+	var b = n * n;
+	alert("The square of "+n+" is "+b);
+}
+
+var z = square();
+*/
+
+//Task 6
+/*
+function factorial(n) {
+	//base case
+	if ( n == 0 || n == 1){
+		return 1;
+	}
+	else {
+		return n * factorial(n-1);
+	}
+}
+var a = prompt("enter a number");
+let n = parseInt(a);
+var ans = factorial(n);
+alert("the factorial of "+n+ " is "+ans);
+*/
+
+
+//Task 7
+function counter(a, b) {
+		for (i = q; i <= y; i++) {
+			document.write(i);
+		}
+var w = prompt("enter a start number");
+var x = prompt("enter an end number");
+let a = parseInt(w);
+let b = parseInt(x);
+var z = counter(a, b);
+
+
+}
+
+
+
+
+
+
+
+
+
+
